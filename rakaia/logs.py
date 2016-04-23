140ebeeab70e079a30551ebba48b7cf2e9c442f9
Nathaniel J. Smith <njs@pobox.com>
2016-04-22 23:37:46 -0700

    Use @async_generator to dramatically simplify tailable files

--- rakaia/logs.py
+++ rakaia/logs.py
@@ -5,6 +5,7 @@ import collections
 import asyncio
 
 from .util import LocklessCondition
+from .async_generator import async_generator, yield_
 
 # Utility classes for streaming a file onto disk, while letting other code in
 # the same process simultaneously stream the file back off of disk.
@@ -44,74 +45,46 @@ class TailableFileWriter:
             os.unlink(self._path)
 
     def aiter_any(self, *, max_chunk_size=16384):
-        return _TailableFileChunks(self,
-                                   open(self._path, "rb"),
-                                   max_chunk_size)
+        # Have to make sure the file gets opened before we enter the
+        # coroutine, because if we do it in the coroutine then the actual
+        # opening might be delayed for an arbitrarily long time, by which
+        # point the file could have been unlinked.
+        read_handle = open(self._path, "rb")
+        return self._aiter_any_impl(read_handle, max_chunk_size)
 
-    def aiter_lines(self, max_chunk_size=16384):
-        return _TailableFileLines(self.aiter_any(max_chunk_size=max_chunk_size))
+    @async_generator
+    async def _aiter_any_impl(self, read_handle, max_chunk_size):
+        with read_handle:
+            while True:
+                if self._bytes_available > read_handle.tell():
+                    await yield_(read_handle.read(max_chunk_size))
+                elif self._closed:
+                    raise StopAsyncIteration
+                else:
+                    await self._condition.wait()
 
-# Just an async iterator, no fancy stream interface with read/close/etc.
-class _TailableFileChunks:
-    def __init__(self, writer, read_handle, max_chunk_size):
-        self._writer = writer
-        self._read_handle = read_handle
-        self._max_chunk_size = max_chunk_size
+    def aiter_lines(self, *, max_chunk_size=16384):
+        # See comment on aiter_any
+        aiter_any = self.aiter_any(max_chunk_size=max_chunk_size)
+        return self._aiter_lines_impl(aiter_any)
 
-    async def __aiter__(self):
-        return self
-
-    async def __anext__(self):
-        while True:
-            if self._writer._bytes_available > self._read_handle.tell():
-                return self._read_handle.read(self._max_chunk_size)
-            elif self._writer._closed:
-                raise StopAsyncIteration
-            else:
-                await self._writer._condition.wait()
-
-class _TailableFileLines:
-    def __init__(self, chunk_aiter):
-        self._chunk_aiter = chunk_aiter
-        # already complete lines, ordered from last-to-first (so .pop() will
-        # return the next one)
-        self._pending_queue = []
-        # partial line carried over between chunks
-        self._partial = io.BytesIO()
-
-    async def __aiter__(self):
-        return self
-
-    async def _refill(self):
-        "After calling this, _pending_queue will be empty iff we are at EOF"
-        # no complete lines available -- need to get more data
-        async for chunk in self._chunk_aiter:
+    @async_generator
+    async def _aiter_lines_impl(self, aiter_any):
+        # The code with BytesIO is a bit over-complicated, because we're
+        # trying to avoid quadratic behavior when we get a very long line
+        # split up into lots and lots of independent chunks (e.g., a giant
+        # string of "......" as output by a test suite).
+        trailing = io.BytesIO()
+        async for chunk in aiter_any:
             lines = chunk.split(b"\n")
-            self._partial.write(lines[0])
+            trailing.write(lines[0])
             if len(lines) == 1:
-                # no line break here
                 continue
             else:
-                lines[0] = self._partial.getvalue()
-                self._partial = io.BytesIO()
-                self._partial.write(lines.pop())
-                lines.reverse()
-                self._pending_queue = lines
-                return
-        # hit end-of-file
-        last_line = self._partial.getvalue()
-        self._partial = io.BytesIO()
-        if last_line:
-            self._pending_queue = [last_line]
-        else:
-            self._pending_queue = []
-
-    async def __anext__(self):
-        if not self._pending_queue:
-            await self._refill()
-        if not self._pending_queue:
-            raise StopAsyncIteration
-        else:
-            return self._pending_queue.pop()
-
-
+                lines[0] = trailing.getvalue()
+                trailing = io.BytesIO()
+                trailing.write(lines.pop())
+            for line in lines:
+                await yield_(line)
+        if trailing.getvalue():
+            await yield_(trailing.getvalue())
